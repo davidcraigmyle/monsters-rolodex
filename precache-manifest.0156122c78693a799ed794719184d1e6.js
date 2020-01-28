dee02c878b36f56f2f46cf4454f2344e569ff5af
David Craigmyle <davidcraigmyle@hotmail.com>
2020-01-28 21:10:00 +0000

    Updates

---
+++ precache-manifest.0156122c78693a799ed794719184d1e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee7f9bc088bc3ccff8ba812d4ae0fcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e2603a10e5f04941220",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b49ede963a0f2ff5b4ef",
+    "url": "/static/js/2.62dce5ae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.62dce5ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e2603a10e5f04941220",
+    "url": "/static/js/main.1de0b303.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
